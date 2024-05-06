6a6e359062c3ae73d3fcfa805b2a3608c5bba658
Alaa allam <alaaallam@192.168.1.6>
2024-05-06 03:18:13 +0300

    Video

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
+import Video, {VideoRef} from 'react-native-video';
 
 export default function App() {
+  const background = 'https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8';
+ //const background = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <View style={{flex:1}}>
+      <Text>VIDEO</Text>
+    <Video 
+    // Can be a URL or a local file.
+    source={{uri:background}}
+    // Store reference  
+    // Callback when remote video is buffering                                      
+    muted={true}
+    // Callback when video cannot be loaded              
+    onError={(error)=>{
+      console.log(error)
+    }}               
+    style={{width:500,height:300}}
+   />
+   </View>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
